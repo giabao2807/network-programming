7ce0911a7f089913182d4cfc77d012fe9b6aea29
GiaBaone <102190252@sv1.dut.udn.vn>
2021-12-01 22:13:53 +0700

    udp handle String 1.12.21

---
+++ prepareExam/src/udphandlestring/StringClient.java
@@ -0,0 +1,135 @@
+package udphandlestring;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StringClient extends javax.swing.JFrame {
+    
+    DatagramSocket clientSocket;
+    DatagramPacket sendPacket;
+    DatagramPacket receivePacket;
+
+    public StringClient() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea = new javax.swing.JTextArea();
+        jTextField = new javax.swing.JTextField();
+        jButton = new javax.swing.JButton();
+        jLabel = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea.setColumns(20);
+        jTextArea.setRows(5);
+        jScrollPane1.setViewportView(jTextArea);
+
+        jButton.setText("Enter");
+        jButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel.setText("Bài 1: Xử lý chuỗi UDP");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 216, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void jButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            
+            sendData = jTextField.getText().getBytes();
+            
+            this.clientSocket = new DatagramSocket();
+            
+            this.sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7778);
+            this.clientSocket.send(sendPacket);
+            
+            this.receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            this.clientSocket.receive(receivePacket);
+            
+            String str = new String(receivePacket.getData());
+            jTextArea.setText(str);
+        } catch (IOException ex) {
+            Logger.getLogger(StringClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(StringClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(StringClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(StringClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(StringClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new StringClient().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton;
+    private javax.swing.JLabel jLabel;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea;
+    private javax.swing.JTextField jTextField;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ prepareExam/src/udphandlestring/StringServer.java
@@ -0,0 +1,112 @@
+package udphandlestring;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class StringServer {
+    
+    public static DatagramSocket datagramSocket;
+    
+    public static void main(String[] args) {
+        try {
+            datagramSocket = new DatagramSocket(7778);
+            System.out.println("Server is started!");
+            while (true) {  
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                datagramSocket.receive(receivePacket);
+                Thread thread = new Thread(new Client(receivePacket));
+                thread.start();
+                
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
+
+class Client implements Runnable {
+
+    private DatagramPacket datagramPacket;
+
+    public Client(DatagramPacket receivePacket) {
+        this.datagramPacket = receivePacket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            byte[] sendData = new byte[1024];
+            InetAddress IPAddress = datagramPacket.getAddress();
+            int port = datagramPacket.getPort();
+            
+            String input = new String(datagramPacket.getData());
+            String output = "Chuoi in hoa: " + chuoiHoa(input).trim() + "\n"
+                    + "Chuoi thuong: " + chuoiThuong(input).trim() + "\n"
+                    + "Chuoi vua hoa vua thuong: " + chuoiVuaHoaVuaThuong(input).trim() + "\n"
+                    + "So tu cua chuoi: " + soTu(input);
+            
+            sendData = output.getBytes();
+            
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            StringServer.datagramSocket.send(sendPacket);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String chuoiVuaHoaVuaThuong(String str) {
+        char c;
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            c  = str.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                c = (char) (c-32);
+            } else if (c >= 'A' && c <= 'Z') {
+                c = (char) (c+32);
+            }
+            result += c;
+        }
+        return result;
+    }
+
+    public String chuoiHoa(String str) {
+        char c;
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            c  = str.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                c = (char) (c-32);
+            }
+            result += c;
+        }
+        return result;
+    }
+
+    public String chuoiThuong(String str) {
+        char c;
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            c  = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) (c+32);
+            }
+            result += c;
+        }
+        return result;
+    }
+
+    public int soTu(String str) {
+        int result = 0;
+        for (String s : str.split(" ")) {
+            if (!s.trim().equals("")) {
+                result++;
+            }
+        }
+        return result;
+    }
+    
+}
